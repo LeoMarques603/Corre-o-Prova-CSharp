f208aadc7f59c9dd3e8415b65e52898addec4768
Leonardo Marques <sandstorm603@gmail.com>
2018-10-16 20:34:16 -0300

    Add files via upload

---
+++ Correção Prova/Correção Prova/IMC.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Correção_Prova
+{
+    class IMC
+    {
+        private double peso;
+        private double altura;
+
+
+        public IMC()
+        {
+            this.peso = 0.0;
+            this.altura = 0.0;
+        }// Fim IMC
+
+        public IMC(double peso, double altura)
+        {
+            this.peso = peso;
+            this.altura = altura;
+        }
+
+        public double Calcular(double peso, double altura)
+        {
+            return peso / (altura * altura);
+        }
+
+        public double Classificar(double imc)
+        {
+            return 0.0;
+        }
+    }
+}
