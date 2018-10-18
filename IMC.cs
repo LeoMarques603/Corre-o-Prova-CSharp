2df4e8fe155dbff71d57be3c353423b6a461a994
Leonardo Marques <sandstorm603@gmail.com>
2018-10-18 20:00:08 -0300

    Add files via upload

---
+++ IMC.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Correção_Prova
+{// Questão 02
+    class IMC
+    {
+        private double peso;
+        private double altura;
+
+
+        public IMC()
+        {
+            this.peso = 80.0;
+            this.altura = 2.0;
+        }// Fim IMC
+
+        public IMC(double peso, double altura)
+        {
+            this.peso = peso;
+            this.altura = altura;
+        }
+
+        public double Calcular(double peso, double altura)
+        {
+            return peso / (altura * altura);
+        }
+
+        public double Classificar(double imc)
+        {
+            if (Calcular < 16.0)
+                return Console.WriteLine("Magreza Grave");
+
+            if (16.0 > Calcular < 17.0)
+                return Console.WriteLine("Magreza Moderada");
+
+            if (17.0 > Calcular < 18.5)
+                return Console.WriteLine("Magreza Leve");
+
+            if (18.5 > Calcular < 25.0)
+                return Console.WriteLine("Saudável");
+
+            if (25.0 > Calcular < 30.0)
+                return Console.WriteLine("Sobrepeso");
+
+            if (30.0 > Calcular < 35.0)
+                return Console.WriteLine("Obesidade Grau I");
+
+            if (35.0 > Calcular < 40.0)
+                return Console.WriteLine("Obesidade Grau II (Severa)");
+
+            if (Calcular > 40)
+                return Console.WriteLine("Obesidade Grau III (Mórbida)");
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Correção_Prova
+{
+    class Program
+    {
+        //Questão 1
+        static int Maior(int a, int b)
+        {
+            if (a > b)
+            { return a; }
+
+            else
+            { return b; }
+        }// Fim método maior
+
+        //Questão 3
+        static bool Par(int numero)
+        {
+            if (numero % 2 == 0)
+                return true;
+            else
+                return false;
+        }// Fim método par
+
+        static void Main()
+        {
+            Console.ReadKey();
+        }
+
+    }// Fim classe program
+
+}//Fim namespace
